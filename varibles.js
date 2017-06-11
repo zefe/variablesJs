ed69b4e7f509200d9d4e6a2854998ed86d268b60
zefetorres <zefetorres@icloud.com>
2017-06-11 17:50:56 -0500

    declaracion de variables

---
+++ varibles.js
@@ -0,0 +1,19 @@
+var name = "zefe"
+
+function saludar() {
+	var name = "rocio"
+	console.log(`hola ${name}`)
+}
+
+saludar()
+console.log(`La variable name tiene el valor ${name}`)
+
+
+function goodJob() {
+	const saludo = "Buen trabajo"
+
+	for(let i=0; i<10; i++) {
+		console.log(`zefe ${saludo}`)
+	}
+}
+goodJob()
